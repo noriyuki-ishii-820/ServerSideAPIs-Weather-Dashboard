904ee94bb4ac9fff6e7d614d2dc098714b7d7176
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-09-18 17:59:20 +1000

    js bug fixed

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,17 @@
 var cityArr = [];
 
+function displayWeather(){
+    alert("Hi!");
+
+}
+
  $("#search").on("click", function(event){
         event.preventDefault();
         var citySearched = $("#userInput").val().trim();
-       
+
+        if(!citySearched){
+            alert("Your input is not valid. Please try again!");
+        } else {
         cityArr.push(citySearched);
         
         console.log(cityArr);
@@ -18,16 +26,21 @@ var cityArr = [];
         $(".list-group").empty();
 
         //create the list of cities searched
-
-        for (var i = 0; i < cityArr.length; i++){
-            var li = $("<li>");
-            li.addClass("list-group-item");
-            li.text(cityArr[i]);
-            $(".list-group").append(li);
-        };
+        if (!citySearched) {
+            alert("Your input is not valid. Please try again!");
+        } else {
+            for (var i = 0; i < cityArr.length; i++){
+                var li = $("<li>");
+                li.addClass("list-group-item");
+                li.text(cityArr[i]);
+                $(".list-group").append(li);
+        }};
+        }
+        // clears the text input 
 
         $("#userInput").val('');
 
+        displayWeather();
 })
 
 
